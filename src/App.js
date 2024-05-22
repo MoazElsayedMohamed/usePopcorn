c9743ecbde527db13872bc0ef399bae65f3292c3
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-05-22 23:49:37 +0300

    feat: :memo: create Main comonent

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
-function App() {}
+import Main from "./components/Main";
+import NavBar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Main />
+    </div>
+  );
+}
 
 export default App;
