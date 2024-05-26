05968a1a03be261a1d2fbd0bdfbd02e2fffd17b2
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-05-26 18:52:07 +0300

    feat: :zap: add movie details

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Box from "./components/ListBox";
 import MovieList from "./components/MovieList";
 import WatchedSummary from "./components/WatchedSummary";
 import WatchedMovieList from "./components/WatchedMovieList";
+// import StarRating from "./components/StarRating";
+import MovieDetails from "./components/MovieDetails";
+import Loader from "./components/Loader";
 
 const KEY = "91356a83";
 
@@ -92,16 +95,73 @@ function App() {
   );
 }
 
-function MovieDetails({ selectedId, onClose }) {
-  return (
-    <div className="details">
-      <button className="btn-back" onClick={onClose}>
-        &larr;
-      </button>
-      {selectedId}
-    </div>
-  );
-}
+// function MovieDetails({ selectedId, onClose }) {
+//   const [movie, setMovie] = useState({});
+//   const [isLoading, setIsLoading] = useState(false);
+
+//   const {
+//     Title: title,
+//     Poster: poster,
+//     Runtime: runtime,
+//     imdbRating,
+//     Plot: plot,
+//     Released: released,
+//     Actors: actors,
+//     Director: director,
+//     Genre: genre,
+//   } = movie;
+
+//   useEffect(() => {
+//     setIsLoading(true);
+//     async function getMovieDetails() {
+//       const res = await fetch(
+//         `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+//       );
+//       const data = await res.json();
+//       setMovie(data);
+//       setIsLoading(false);
+//     }
+//     getMovieDetails();
+//   }, [selectedId]);
+
+//   return (
+//     <>
+//       {isLoading ? (
+//         <Loader />
+//       ) : (
+//         <div className="details">
+//           <header>
+//             <button className="btn-back" onClick={onClose}>
+//               &larr;
+//             </button>
+//             <img src={poster} alt={`Poster of ${movie} movie`} />
+//             <div className="details-overview">
+//               <h2>{title}</h2>
+//               <p>
+//                 {released} &bull; {runtime}
+//               </p>
+//               <p>{genre}</p>
+//               <p>
+//                 <span>⭐</span>
+//                 {imdbRating} IMDb rating
+//               </p>
+//             </div>
+//           </header>
+//           <section>
+//             <div className="rating">
+//               <StarRating maxRating={10} size={24} />
+//             </div>
+//             <p>
+//               <em>{plot}</em>
+//             </p>
+//             <p>Starring {actors}</p>
+//             <p>Directed by {director}</p>
+//           </section>
+//         </div>
+//       )}
+//     </>
+//   );
+// }
 
 function ErrorMessage({ message }) {
   return (
@@ -109,10 +169,6 @@ function ErrorMessage({ message }) {
       <span>⛔</span> {message}
     </p>
   );
-}
-
-function Loader() {
-  return <div className="loader">Loading...</div>;
 }
 
 export default App;
