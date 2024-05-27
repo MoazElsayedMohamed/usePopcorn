65b8a71ced9a4a667b508deec1b6966385d8f19e
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-05-27 15:51:36 +0300

    feat: :sparkles: store data in localstorage with callback initial state

--- src/App.js
+++ src/App.js
@@ -15,10 +15,15 @@ const KEY = "91356a83";
 function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+
+  // const [watched, setWatched] = useState([]);
+  const [watched, setWatched] = useState(function () {
+    const storeData = localStorage.getItem("watched");
+    return JSON.parse(storeData);
+  });
 
   const handleSelectMovie = (id) => {
     setSelectedId((selectedId) => (selectedId === id ? null : id));
@@ -35,6 +40,10 @@ function App() {
   const handleDeleteWatched = (id) => {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   };
+
+  useEffect(() => {
+    localStorage.setItem("watched", JSON.stringify(watched));
+  }, [watched]);
 
   const controller = new AbortController();
 
@@ -74,6 +83,8 @@ function App() {
         setError("");
         return;
       }
+
+      handleCloseMovie();
       fetchMovie();
 
       return () => controller.abort();
